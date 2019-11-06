2c39fd1f390d8f999687169985ab776d437a891a
unknown <jzapatam13@gmail.com>
2019-11-06 20:25:52 +0100

    Separating in modules

--- module/esios_operator.py
+++ module/esios_operator.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  9 17:04:53 2019
-
-@author: jzapa
-"""
 from urllib.error import HTTPError
 import datetime
 import html
@@ -12,92 +6,9 @@ import pandas as pd
 import re
 import urllib
 
-
-class EsiosHook():
-    """
-    Interact with Esios API
-    :param token: personal authentication to use esios api
-    :type token: str
-    :param base_url: url to connect to esios api
-    :type: str
-    """
-
-    def __init__(
-        self,
-        token,
-        base_url
-    ):
-        self.token = token
-        self.base_url = base_url
-
-    def _get_headers_(self):
-        """
-        Create the specific headers to connect with esios API
-        """
-        headers = dict()
-        headers["Accept"] = ("application/json; "
-                             "application/vnd.esios-api-v1+json")
-        headers["Content-Type"] = "application/json"
-        headers["Host"] = "api.esios.ree.es"
-        headers['Authorization'] = 'Token token=\"' + self.token + '\"'
-        headers["Cookie"] = ""
-        return headers
-
-    def _get_conn_(self, indicator=None,
-                   start_date_esios=None,
-                   end_date_esios=None):
-        base_url = self.base_url
-        if indicator:
-            if not start_date_esios or not end_date_esios:
-                raise ValueError(
-                    "If use indicator id, you'll need to use some dates")
-            else:
-                try:
-                    datetime.datetime.strptime(
-                        start_date_esios, '%Y-%m-%dT%H:%M:%S')
-                    datetime.datetime.strptime(
-                        end_date_esios, '%Y-%m-%dT%H:%M:%S')
-                    if start_date_esios >= end_date_esios:
-                        raise ValueError(
-                            ("end_date_esios must be upper than "
-                             "start_date_esios {} > {}"
-                             .format(start_date_esios, end_date_esios)))
-                    else:
-                        base_url = "{0}/{1}?start_date={2}&end_date={3}".format(
-                            base_url,
-                            str(indicator),
-                            start_date_esios,
-                            end_date_esios)
-                except ValueError:
-                    raise ValueError(
-                        "Incorrect date format, "
-                        "should be yyyy-mm-dd'T'hh:mm:ss")
-        else:
-            pass
-        req = urllib.request.Request(
-            base_url, headers=self._get_headers_())
-        return req
-
-    def check_and_run(self, indicator=None,
-                      start_date_esios=None,
-                      end_date_esios=None):
-        req = self._get_conn_(indicator, start_date_esios, end_date_esios)
-        try:
-            response = urllib.request.urlopen(req)
-        except HTTPError as error:
-            raise error
-        except Exception:
-            print("UnControlled Error")
-            raise
-        try:
-            json_data = response.read().decode('utf-8')
-        except:
-            json_data = response.readall().decode('utf-8')
-        result = json.loads(json_data)
-        return result
-
-
-class EsiosIndicatorsOperator(BaseOperator):
+from esios_hook import EsiosHook
+from postgres_hook import PostgresEsiosHook
+class EsiosOperator():
     """
     Get table about Indicators Operators from esios Api
     :param token: personal authentication to use esios api
@@ -117,8 +28,8 @@ class EsiosIndicatorsOperator(BaseOperator):
         self.base_url = base_url
         self.table = table
 
-    def execute(self, context):
-        postgres = PostgresEsiosHook()
+    def load_description_table(self):
+        postgres = PostgresHook()
         esios = EsiosHook(self.token, self.base_url)
         result = esios.check_and_run()
         df = pd.DataFrame(data=result["indicators"], columns=[
@@ -126,7 +37,7 @@ class EsiosIndicatorsOperator(BaseOperator):
         df['description'] = df['description'].apply(
             lambda x: re.sub("<[(/p)(/b)pb]>", "", html.unescape(x))
             .replace('</p>', '').replace("</b>", ""))
-
+        return df
         postgres.load_df_esios(df, self.table, if_exists="replace")
         print("Indicators Description Data has been loaded correctly")
 
